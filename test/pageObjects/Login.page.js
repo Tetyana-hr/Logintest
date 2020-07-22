b8e94068bc1bd27bbc8614fcb17a6ef4aeb66295
tania <thrivas@netw.fr>
2020-07-22 10:10:53 +0300

    add files to project

---
+++ test/pageObjects/Login.page.js
@@ -0,0 +1,17 @@
+class Login {
+    get $email () { return $('input[name="username"]'); }
+    get $password () { return $('input[name="password"]'); }
+    get $select () { return $('select[name="lang"'); }
+    get $submit () { return $('button[type="submit"'); }
+    
+
+    login ({ email, password, select }) {
+        this.$email.setValue(email);
+        this.$password.setValue(password);
+        this.$select.setValue(select);
+
+        this.$submit.click();
+    }
+}
+
+module.exports = Login;
