3c1bf4fb43ae0d98547a4d3ec4995b57d0bbec88
tania <thrivas@netw.fr>
2020-08-10 17:57:12 +0300

    ending of call

--- test/Calling/sipCall.js
+++ test/Calling/sipCall.js
@@ -4,14 +4,14 @@ class sipCall {
     get $numpad () { return $('div[id="numpad"]'); }
     get $phone () { return $('input[type="tel"]'); }
     get $call () { return $('div[class="keypad-button"]'); }
-    // get $callEnd () { return $('button[title="Hangup"]'); }
+    get $callEnd () { return $('button[data-terminate]'); }
     
     call ({ phone }) {
         this.$numpad.click();
         this.$phone.setValue(phone);
         this.$call.click();   
-        // browser.pause(5000);
-        // this.$callEnd.click();   
+        browser.pause(5000);
+        this.$callEnd.click();   
      }
 }
 
