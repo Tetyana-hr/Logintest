4c83133720df9e3f574cb0d0122b5857f2b82231
Tetyana-hr <grivastg@gmail.com>
2020-10-28 17:20:18 +0200

    search form

---
+++ test/specs/Pause.js
@@ -0,0 +1,22 @@
+// const validUser = require('../../SecurityData/SecData.js'); 
+
+// describe('Button "Add contact group" is clickable', function () {
+//     beforeEach(function() {
+//         browser.url('./');
+//     });
+
+//     //  ===================================================================================
+    
+//     it('should let you login with valid credentials', function () {
+//         login.login(validUser);
+//         browser.pause(10000);
+        
+//     });
+
+//     // it('Button "Add contact group" is clickable ', function () {
+//     //     this.timeout(20000);
+//     //     browser.pause(7000);
+//     //     browser.click('#button]');
+//     //     browser.pause(10000);
+//     // });
+// });
