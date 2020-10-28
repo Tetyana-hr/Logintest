fed54b7ce22a3f7bad18e68c10fbac451f4f1bb8
Tetyana-hr <grivastg@gmail.com>
2020-10-28 17:02:03 +0200

    New files and search form

---
+++ test/Calling/sipCall.js
@@ -0,0 +1,19 @@
+class sipCall {
+    // get $email () { return $('input[name="username"]'); }
+    
+    get $numpad () { return $('div[id="numpad"]'); }
+    get $phone () { return $('input[type="tel"]'); }
+    get $call () { return $('div[class="keypad-button"]'); }
+    get $callEnd () { return $('button[data-terminate]'); }
+    
+    call ({ phone }) {
+        this.$numpad.click();
+        this.$phone.setValue(phone);
+        this.$call.click();   
+        browser.pause(5000);
+        this.$callEnd.click();   
+      
+     }
+}
+
+module.exports = sipCall;
---
+++ test/specs/login.spec.js
@@ -0,0 +1,39 @@
+const validUser = require('../../SecurityData/SecDataSecond.js'); 
+const Login = require('../pageObjects/Login.page.js');
+const login = new Login();
+const SipCall = require('../Calling/sipCall.js');
+const sipCall = new SipCall();
+
+// Move assert, expect, should to wdio.conf
+
+const callPhone = {
+    phone: '33970758487',
+};
+
+describe('Login Page and calling', function () {
+    beforeEach(function() {
+        browser.url('./');
+    });
+    it('should let you login with valid credentials', function () {
+        login.login(validUser);
+        browser.pause(10000);
+        
+    });
+
+    it('should be title Client', function () {
+        let title = browser.getTitle();
+        assert.equal(title, 'Client'); // the same
+        expect(title).to.equal('Client'); // the same
+        title.should.equal('Client'); // the same
+        console.log('Title is: ' + title);
+        browser.pause(10000);
+    });
+  
+        
+    //     it('Make a call', function(){
+    //     sipCall.call(callPhone);            
+    //     browser.pause(10000);
+       
+    // });
+
+})
